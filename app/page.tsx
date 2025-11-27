6e10b330a151c651e1798fd2e4e398fdcb192ac9
Nimit <521nimitprajapati98@gmail.com>
2025-11-27 17:02:51 +0530

    Updated Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/hero";
 import { Navbar1 } from "@/components/navbar1";
+import Aboutus from "@/components/About us";
 import Image from "next/image";
 
 export default function Home() {
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,43 +1,30 @@
-import CardSwap, { Card } from "@/components/CardSwap"
-import Image from "next/image";
+"use client";
+import React from "react";
+import { BackgroundRippleEffect } from "@/components/ui/background-ripple-effect";
+import { Button } from "./ui/moving-border";
+
+
 
 export default function Hero() {
   return (
-    <div className="min-h-full w-[95%] flex items-center justify-center p-8">
-      <div className="w-full max-w-7xl grid grid-cols-1 lg:grid-cols-2 gap-12 items-center ">
-        
-        <div className="text-accent-foreground space-y-3 border flex flex-col items-center md:items-start">
-          <h1 className="text-5xl md:text-6xl font-bold leading-12 md:leading-tight">
-            Simplifying Modernization
-          </h1> 
-          <p className="text-xl text-gray-400">
-            Our tailored, experience-focused solutions span a diverse range of services. We enable organizations to achieve measurable transformation and accelerate success by nearly 80%.
-          </p>
-        </div>
-
-        <div className="relative" style={{ height: '450px' }}>
-          <CardSwap
-            cardDistance={40}
-            verticalDistance={40}
-            delay={3500}
-            skewAmount={4}
-            easing="elastic"
-          >
-
-            <Card>
-                    <Image src="/image-5.jpg" alt="image-1" width={500} height={500} className="object-cover rounded-xl "/>
-            </Card>
-
-            <Card>
-                  <Image src="/image-2.jpg" alt="image-3  " width={500} height={500} className="object-cover rounded-xl"/>
-            </Card>
-
-            <Card>
-                  <Image src="/image-1.jpg" alt="image-3" width={500} height={500} className="object-cover rounded-xl"/>
-            </Card>
-         </CardSwap>
-
-        </div>
+    <div className="relative flex min-h-screen w-full flex-col items-start justify-start overflow-hidden">
+      <BackgroundRippleEffect />
+      <div className="mt-60 w-full">
+        <h2 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-neutral-800 md:text-4xl lg:text-7xl dark:text-neutral-100">
+          Simplifying Modernization
+        </h2>
+        <p className="relative z-10 mx-auto mt-4 max-w-xl text-center text-neutral-800 dark:text-neutral-500">
+          Our tailored, experience-focused solutions span a diverse range of services. We enable organizations to achieve measurable transformation and accelerate success by nearly 80%.
+        </p>
+<div className="mt-6 flex justify-center">
+  <div className="relative w-[180px] h-[60px]">
+      <Button
+        borderRadius="1.75rem"
+        className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"      >
+        Learn More
+      </Button>
+  </div>
+</div>
 
       </div>
     </div>
